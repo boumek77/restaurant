c2b741b82041326e4c4f5039c66a119a9779250a
boumek77 <mehdiboumekki@gmail.com>
2017-11-23 19:37:37 +0100

    first6

---
+++ app/assets/javascripts/resto.js
@@ -0,0 +1,8 @@
+
+function button() {
+    var x = document.getElementByUrl("http://www.opera-restaurant.fr/fr/");
+    x.style.fontSize = "25px";
+    x.style.color = "red";
+}
+
+alert("Bonne dégustation ;)")
